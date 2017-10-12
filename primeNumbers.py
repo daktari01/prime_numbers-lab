c3ca2bb040e19843dd5c9f6ee291e4d3451ffb4a
Daktari <dindijjames@gmail.com>
2017-10-12 01:38:36 +0000

    First attempt,fails three tests

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,13 @@
+def primeNumbers(n):
+    prime_list = []
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+        return True
+    if i == True:
+        prime_list.append(i)
+    else:
+        return False
 
