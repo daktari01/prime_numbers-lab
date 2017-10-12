4087c51132b14e6277b9d294d130845612605c03
Daktari <dindijjames@gmail.com>
2017-10-12 02:39:51 +0000

    Optimized the code, though still fails 3/5 tests

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,13 +1,13 @@
 def primeNumbers(n):
     prime_list = []
-    if n <= 1:
-        return False
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-        return True
-    if i == True:
-        prime_list.append(i)
-    else:
-        return False
-
+    for x in range(2, n + 1):
+        is_prime = True
+        for y in range(2, int(x ** 0.5) + 1):
+            if x % y == 0:
+                is_prime = False
+                break
+        if is_prime:
+            #print(x)
+            prime_list.append(x)
+    print(prime_list)       
+primeNumbers(10)
